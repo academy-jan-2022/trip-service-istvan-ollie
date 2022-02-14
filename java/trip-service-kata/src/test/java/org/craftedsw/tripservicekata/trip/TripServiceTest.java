f537074dfbc9c3b7582c6378ad20132a05fe7daf
oawood93 <oa.wood@outlook.com>
2022-02-14 11:51:40 +0000

    Green test for trips service, now full test coverage

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -3,38 +3,62 @@ package org.craftedsw.tripservicekata.trip;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Rule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class TripServiceTest {
 
 	private static final User GUEST = null;
-	private static final User NOT_LOGGED_IN_USER = null;
+	private User LOGGED_IN_USER_RESULT = null;
+	private TripServiceTestable tripService;
 
-
-	@Rule
-	public final ExpectedException exception = ExpectedException.none();
-
+	@BeforeEach
+	void setUp(){
+		tripService = new TripServiceTestable();
+		LOGGED_IN_USER_RESULT = null;
+	}
 
 	@Test
 	public void
 	should_throw_exception_when_no_user_logged_in(){
-		TripServiceTestable tripService = new TripServiceTestable();
 		User user = GUEST;
-
 		assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
+	}
+
+
+	@Test
+	public void
+	should_return_trips_of_users_friends(){
+		var loggedInUser = new User();
+		var userToGetTripsFor = new User();
+		LOGGED_IN_USER_RESULT = loggedInUser;
+
+		userToGetTripsFor.addFriend(loggedInUser);
+		userToGetTripsFor.addTrip(new Trip());
+
+		var userTrips = tripService.getTripsByUser(userToGetTripsFor);
+		assertEquals(1, userTrips.size());
+
 	}
 
 	public class TripServiceTestable extends TripService {
 
 		@Override
 		protected User getLoggedUser() {
-			return NOT_LOGGED_IN_USER;
+			return LOGGED_IN_USER_RESULT;
 		}
 
+		@Override
+		protected List<Trip> getTripsFor(User user){
+			return List.of(new Trip());
+		}
 	}
 
 
